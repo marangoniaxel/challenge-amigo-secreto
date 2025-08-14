a772734cde4e3c547f43a1ad3e3be3d9a4d2e7da
Axel Marangoni <marangoniaxelerie@gmail.com>
2025-08-14 05:18:16 -0300

    Challenge completado: Agregar y sortear amigos

--- app.js
+++ app.js
@@ -1,1 +1,71 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// creando el challenge!
+
+let amigos = [];
+//lista para almacenar a los amigos que van a ser sorteados.
+const inputNombre = document.getElementById("amigo");
+const botonAgregar = document.getElementById("addAmigo");
+const listaHTML = document.getElementById("listaAmigos");
+
+const botonSortear = document.getElementById("sorteoAmigo");
+const listaSorteo = document.getElementById("resultado");
+//conectar las funciones html y js.
+
+
+
+function agregarAmigo() {
+
+    let nombre = inputNombre.value.trim();
+    
+    if (nombre === ""){
+        alert("Por favor ingrese un nombre.")
+        return;
+    }
+
+    if (amigos.includes(nombre)) {
+        alert("El nombre ya esta en la lista.")
+        return;
+    }
+
+    amigos.push(nombre);
+
+
+    let li = document.createElement("li");
+    li.textContent = nombre;
+    listaHTML.appendChild(li);
+
+    inputNombre.value = "";
+    inputNombre.focus();
+    
+}
+
+//funcion para agregar nombres a la lista
+
+function sortearAmigo(){
+
+    if (amigos.length === 0){
+        return alert("No se puede sortear sin participantes.");
+        
+    }
+
+    let sorteo = Math.floor(Math.random() * amigos.length);
+    let ganador = amigos[sorteo];
+    listaSorteo.innerHTML = "";
+    let li = document.createElement("li")
+    li.textContent = ganador;
+    listaSorteo.appendChild(li);
+}
+//funciona para realizar el sorteo.
+
+botonAgregar.addEventListener("click",agregarAmigo);
+
+inputNombre.addEventListener ("keydown", function(event) {
+    if (event.key === "Enter"){
+        event.preventDefault();
+        agregarAmigo();
+    }
+});
+// da la funcion al boton añadir y tambien lo hace con el enter para hacerlo mas practico.
+
+
+botonSortear.addEventListener("click", sortearAmigo);
+
